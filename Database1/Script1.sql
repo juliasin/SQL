9b7bc322e1a72afef2031140a9dbff9e8dd5cf39
juliasin <juliasinegina@gmail.com>
2016-10-28 12:43:03 +0300

    Add project files.

---
+++ Database1/Script1.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE TABLE Employees (
+ID INT NOT NULL Primary key, 
+Name CHAR(25) NOT NULL, 
+Surname CHAR(25) NOT NULL, 
+BirthDate DATE NOT NULL, 
+PhoneNumber CHAR(25) NOT NULL, 
+HomeAddress CHAR(25) NOT NULL,
+IDCompany INT NOT NULL);
+
+CREATE TABLE Companies (
+ID INT NOT NULL Primary key, 
+Name CHAR(25) NOT NULL, 
+Surname CHAR(25) NOT NULL, 
+BirthDate DATE NOT NULL, 
+PhoneNumber CHAR(25) NOT NULL, 
+HomeAddress CHAR(25) NOT NULL,
+IDCompany INT NOT NULL)
