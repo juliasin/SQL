021d1581f6c3d13a665021b20c3cfbf77b714f5d
juliasin <juliasinegina@gmail.com>
2016-10-28 14:28:17 +0300

    Project

--- Database1/Script1.sql
+++ Database1/Script1.sql
@@ -1,18 +1,35 @@
-﻿
-CREATE TABLE Employees (
-ID INT NOT NULL Primary key, 
-Name CHAR(25) NOT NULL, 
-Surname CHAR(25) NOT NULL, 
-BirthDate DATE NOT NULL, 
-PhoneNumber CHAR(25) NOT NULL, 
-HomeAddress CHAR(25) NOT NULL,
-IDCompany INT NOT NULL);
+﻿USE     master;
+GO
+IF      DB_ID('test')
+        IS NOT NULL
+        DROP DATABASE test;
+GO
+CREATE  DATABASE test;
+GO
+USE     test;
 
-CREATE TABLE Companies (
+CREATE TABLE test.dbo.Companies (
 ID INT NOT NULL Primary key, 
-Name CHAR(25) NOT NULL, 
-Surname CHAR(25) NOT NULL, 
+Name NVARCHAR(25) NOT NULL,  
+CompanyAddress NVARCHAR(25) NOT NULL);
+
+CREATE TABLE test.dbo.PhoneNumbers (
+ID INT NOT NULL Primary key, 
+PhoneNumber CHAR(25) NOT NULL,  
+EmployeeID int NOT NULL);
+
+CREATE TABLE test.dbo.Employees (
+ID INT NOT NULL Primary key, 
+Name NVARCHAR(25) NOT NULL, 
+Surname NVARCHAR(25) NOT NULL, 
 BirthDate DATE NOT NULL, 
-PhoneNumber CHAR(25) NOT NULL, 
-HomeAddress CHAR(25) NOT NULL,
-IDCompany INT NOT NULL)
+HomeAddress NVARCHAR(25) NOT NULL,
+IDPhone INT NOT NULL,
+IDCompany INT NOT NULL,
+CONSTRAINT fkeyCompany FOREIGN KEY (IDCompany)
+		REFERENCES Companies (ID),
+CONSTRAINT fkeyPhone FOREIGN KEY (IDPhone)
+		REFERENCES PhoneNumbers (ID));
+
+
+
